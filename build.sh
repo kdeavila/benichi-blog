9429ac899ab2ee777c9a68e94ca232a69495bfd9
benichi <cbenitez8@cuc.edu.co>
2024-12-31 11:00:53 -0500

    Change file (build.sh)

--- build.sh
+++ build.sh
@@ -2,7 +2,6 @@ set -o errexit  # Exit on error
 pip install -r requirements.txt
 
 python manage.py migrate
-python manage.py collectstatic --noinput
 if [[$CREATE_SUPERUSER == ]];
 then 
     python manage.py createsuperuser --noinput --email "$DJANGO_SUPERUSER_EMAIL" 
